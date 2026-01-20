1c097024a9fa6e6a7da6b63215003c37c10bf274
Brian Smith <bsmit@redhat.com>
2026-01-20 16:23:27 -0500

    batch the pipeline

--- terraform/sandbox/terraform/pipeline.tf
+++ terraform/sandbox/terraform/pipeline.tf
@@ -158,6 +158,12 @@ resource "aws_codebuild_project" "sandbox_project" {
     type      = "CODEPIPELINE"
     buildspec = "terraform/sandbox/pipeline/buildspec-batch.yml"
   }
+
+  build_batch_config {
+    service_role    = aws_iam_role.codebuild_role.arn
+    compute_type_override = "BUILD_GENERAL1_SMALL"
+    timeout_in_mins = 300
+  }
 }
 
 resource "aws_codebuild_project" "janitor_project" {
@@ -254,6 +260,7 @@ resource "aws_codepipeline" "sandbox_pipeline" {
 
       configuration = {
         ProjectName = aws_codebuild_project.sandbox_project.name
+        BatchEnabled = "true"
       }
     }
   }
