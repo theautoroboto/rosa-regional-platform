ddac0a9df740893b600729c818c51c4e2814a977
Kirk Bater <kirk.bater@gmail.com>
2026-02-20 15:00:19 -0500

    add nightly test stub

---
+++ test/common.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+function waitBeforeExit() {
+  local exit_code=$?
+  echo "Waiting before exiting to ensure that logs are captured."
+  sleep 10
+  exit "$exit_code"
+}
+
+trap waitBeforeExit EXIT
+
+if [ -z "${PROW_JOB_NAME:-}" ]; then
+  echo "PROW_JOB_NAME is not set. Exiting."
+  exit 0
+fi
+
