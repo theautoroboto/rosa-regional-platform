3e681385ce466846bcbf3ce4aeed17d7705a0394
Brian Smith <bsmit@redhat.com>
2026-03-19 15:58:20 -0400

    Fix 1

--- terraform/modules/rhobs-infrastructure/versions.tf
+++ terraform/modules/rhobs-infrastructure/versions.tf
@@ -3,12 +3,12 @@
 # =============================================================================
 
 terraform {
-  required_version = ">= 1.10"
+  required_version = ">= 1.14.3"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = ">= 6.0"
     }
   }
 }
